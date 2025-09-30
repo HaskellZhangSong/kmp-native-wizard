48b3cb439565a17044f8fbe04abe8dfabffe8884
songzh <haskell.songzh@hotmail.com>
2025-09-30 12:05:20 +0800

    add coroutine for linux native example

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -2,6 +2,11 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.*
+
 @Serializable
 private data class Message(
     val topic: String,
@@ -18,4 +23,9 @@ fun main() {
         content = "Hello!"
     )
     println(PrettyPrintJson.encodeToString(message))
+    GlobalScope.launch {
+        delay(3000)
+        println("Coroutine finished after delay")
+    }
+    println("haha")
 }
